bb12c3fd1bf80625b2cb560f326ab4f0dc7b31fb
Dimitrasss7 <yakaklegenda@gmail.com>
2025-10-03 13:08:26 +0300

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 0.5s ease-out',
+      },
+    },
+  },
+  plugins: [],
+}
